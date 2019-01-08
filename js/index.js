fa21115984d0fd94d5ec733c9d49da27dbf39eef
Fradniev <fradniev@hotmail.com>
2019-01-07 21:40:35 -0300

    added smooth transition to other menus and fixed button

--- js/index.js
+++ js/index.js
@@ -1,25 +1,40 @@
 $(document).ready(function(){
 	var oldBackground="main-map";
 	$(".footer-block").click(function(event) {
-		$(".selector").css("left",$(this).data("percentage")+"%");
-		$("."+oldBackground).removeClass('main-background');
-		$("."+oldBackground).addClass('left');
-		if (oldBackground=="main-settings") {
-			$(".main-screen").removeClass('settings');
-			$(".header").removeClass('show-header');
-			$("."+oldBackground).removeClass('background-settings');
-		}
-		oldBackground="main-"+$(this).attr('id');
-		$("."+oldBackground).addClass('main-background');
-		if ($("."+oldBackground).hasClass('left')) {
+		if ($(this).attr('id')!=$("."+oldBackground).attr('id')) {
+			$(".selector").css("left",$(this).data("percentage")+"%");
+			$("."+oldBackground).removeClass('main-background');
+			for (var i = parseInt($(this).attr("data-positionButton"))-1; i >= 0; i--) {
+				if ($('*[data-position="'+i+'"]').hasClass('right')) {
+					$('*[data-position="'+i+'"]').removeClass('right');
+				}
+				$('*[data-position="'+i+'"]').addClass('left');
+			}
+			for (var i = parseInt($(this).attr("data-positionButton"))+1; i <= 5; i++) {
+				if ($('*[data-position="'+i+'"]').hasClass('left')) {
+					$('*[data-position="'+i+'"]').removeClass('left');
+				}
+				$('*[data-position="'+i+'"]').addClass('right');
+			} 
+			/*if ($(this).data("position")<$("."+oldBackground).data("position")) {
+				$("."+oldBackground).addClass('right');
+			} else {
+				$("."+oldBackground).addClass('left');
+			}*/
+			if (oldBackground=="main-settings") {
+				$(".main-screen").removeClass('settings');
+				$(".header").removeClass('show-header');
+				$("."+oldBackground).removeClass('background-settings');
+			}
+			oldBackground="main-"+$(this).attr('id');
+			$("."+oldBackground).addClass('main-background');
 			$("."+oldBackground).removeClass('left');
-		} else {
 			$("."+oldBackground).removeClass('right');
-		}
-		if ($(this).attr('id')=="settings") {
-			$(".main-screen").addClass('settings');
-			$("."+oldBackground).addClass('background-settings');
-			$(".header").addClass('show-header');	
+			if ($(this).attr('id')=="settings") {
+				$(".main-screen").addClass('settings');
+				$("."+oldBackground).addClass('background-settings');
+				$(".header").addClass('show-header');	
+			}
 		}
 	});
 });
