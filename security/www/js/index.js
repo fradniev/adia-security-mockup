4338952027979bf8ede8add94c64cfe25abbcb90
fradniev <fradniev@hotmail.com>
2019-01-30 17:28:04 -0300

    working on getting displaycutouts for css

--- security/www/js/index.js
+++ security/www/js/index.js
@@ -17,11 +17,33 @@ function trace(value)
     console.log(value);
 }
 document.addEventListener('deviceready', function () {
-	AndroidFullScreen.showUnderStatusBar();
-	AndroidFullScreen.showUnderSystemUI();
-	AndroidFullScreen.immersiveMode();
-	//console.log(AndroidFullScreen.getDisplayCutout());
-	console.log(AndroidFullScreen.immersiveHeight());
+
+		// Is this plugin supported?
+AndroidFullScreen.isSupported(successFunction, errorFunction);
+
+// Is immersive mode supported?
+AndroidFullScreen.isImmersiveModeSupported(successFunction, errorFunction);
+
+// The width of the screen in immersive mode
+AndroidFullScreen.immersiveWidth(trace, errorFunction);
+
+// The height of the screen in immersive mode
+AndroidFullScreen.immersiveHeight(trace, errorFunction);
+
+// Hide system UI until user interacts
+AndroidFullScreen.leanMode(successFunction, errorFunction);
+
+// Show system UI
+AndroidFullScreen.showSystemUI(successFunction, errorFunction);
+
+// Extend your app underneath the status bar (Android 4.4+ only)
+AndroidFullScreen.showUnderStatusBar(successFunction, errorFunction);
+
+// Extend your app underneath the system UI (Android 4.4+ only)
+AndroidFullScreen.showUnderSystemUI(successFunction, errorFunction);
+
+// Hide system UI and keep it hidden (Android 4.4+ only)
+AndroidFullScreen.immersiveMode(successFunction, errorFunction);
 });
 window.addEventListener('native.keyboardshow', keyboardUp);
 window.addEventListener('native.keyboardhide', keyboardDown);
